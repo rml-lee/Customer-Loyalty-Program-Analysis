f68fc7becfa0414cf55b5cdbdece68293aca23f2
Raheem Lee <160198611+rml-lee@users.noreply.github.com>
2024-09-20 08:42:38 -0400

    Add files via upload

---
+++ Customer Flights Project.sql
@@ -0,0 +1,232 @@
+# Airline Loyalty Campaign Program Analysis
+
+
+# --------------------------------------------------------------------------------------------------------------
+
+## There are no formal days in this dataset. All analysis will be related to Years & Months!! ##
+
+# ---------------------------------------------------------------------------------------------------------------
+
+
+
+-- 1. What were the most popular loyalty cards during the 2018 promotion?
+SELECT
+    loyalty_card,
+    COUNT(*) AS total_customers
+FROM
+    customer_loyalty_history
+WHERE
+    enrollment_type = '2018 Promotion'
+GROUP BY 1
+ORDER BY 2 DESC;
+
+
+
+-- 2. What is the total amount of enrollment and cancellations between loyalty cards with a 2018 promotion enrollment type?
+SELECT
+    loyalty_card,
+    IF(cancellation_date IS NULL, 'Active', 'Inactive') AS status,
+    COUNT(*) AS total_entries
+FROM
+    customer_loyalty_history
+WHERE
+    enrollment_type = '2018 Promotion'
+GROUP BY 1, 2
+ORDER BY 1 ASC;
+
+
+
+-- 3. Which locations do our most valuable customers come from?
+SELECT
+    country,
+    province,
+    ROUND(AVG(clv), 2) AS avg_clv
+FROM
+    customer_loyalty_history
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+
+
+-- 4. What impact did the 2018 promotion have on the amount of enrollment compared to previous years?
+-- Measure the performance of the 2018 promotion (Feb. - Apr.) compared to previous years.
+SELECT
+    DATE_FORMAT(enrollment_date, '%Y-%m') AS year,
+    COUNT(*) AS amount_enrolled
+FROM
+    customer_loyalty_history
+WHERE
+      MONTH(enrollment_date) BETWEEN 2 AND 4
+  AND YEAR(enrollment_date) BETWEEN 2013 AND 2018
+GROUP BY 1
+ORDER BY 1 ASC;
+
+
+
+-- 5. List the customers who enrolled into a loyalty program and dropped within 1 month of enrollment during 2015.
+SELECT
+    loyalty_number,
+    enrollment_date,
+    cancellation_date
+FROM
+    customer_loyalty_history
+WHERE
+      YEAR(enrollment_date) = 2015
+  AND (TIMESTAMPDIFF(MONTH, enrollment_date, cancellation_date) BETWEEN 0 AND 1
+  AND cancellation_date IS NOT NULL);
+
+
+
+-- 6. Which city's average booking amount exceeded the national average?
+WITH nat_avg AS
+         (SELECT
+              ROUND(AVG(flights_booked), 0) AS avg_booked
+          FROM
+              customer_flight_activity)
+SELECT
+    h.city
+FROM
+    customer_loyalty_history h
+    JOIN customer_flight_activity a
+    ON h.loyalty_number = a.loyalty_number
+GROUP BY 1
+HAVING
+    AVG(a.flights_booked) > (SELECT avg_booked FROM nat_avg)
+ORDER BY 1 ASC;
+
+
+
+-- 7. Which customers traveled the furthest?
+SELECT
+    *
+FROM
+    customer_loyalty_history
+WHERE
+    loyalty_number IN
+    (SELECT
+         loyalty_number
+     FROM
+         customer_flight_activity
+     WHERE
+         distance = (SELECT MAX(distance) FROM customer_flight_activity))
+ORDER BY 4 DESC;
+
+
+
+-- 8. What is the enrollment distribution percentage of Loyalty Cards?
+SELECT
+    loyalty_card,
+    COUNT(loyalty_card) AS card_count,
+    ROUND((COUNT(loyalty_card) / (SELECT COUNT(*) FROM customer_loyalty_history)) * 100, 0) AS enrollment_distribution_percentage
+FROM
+    customer_loyalty_history
+GROUP BY 1;
+
+
+
+-- 9. What was the percentage of monthly aurora card enrollments during the 2018 promotion?
+WITH promo AS
+         (SELECT
+              *
+          FROM
+              customer_loyalty_history
+          WHERE
+              enrollment_type = '2018 Promotion'),
+
+     aurora_promo AS
+         (SELECT
+              *
+          FROM
+              customer_loyalty_history
+          WHERE
+                enrollment_type = '2018 Promotion'
+            AND loyalty_card = 'Aurora')
+SELECT
+    MONTH(p.enrollment_date) AS month,
+    (COUNT(a.loyalty_number) / COUNT(p.loyalty_number)) * 100 AS aurora_percentage
+FROM
+    promo p
+    LEFT JOIN aurora_promo a
+    ON p.loyalty_number = a.loyalty_number
+GROUP BY 1
+ORDER BY 1 ASC;
+
+
+
+-- 10. Which cities had the largest growth and drop in volume of enrollments between Feb. and Mar. of the 2018 promotion?
+WITH cte AS
+         (SELECT
+              MONTH(enrollment_date) AS month,
+              city,
+              COUNT(enrollment_date) AS total,
+              COUNT(enrollment_date) -
+              LAG(COUNT(enrollment_date)) OVER (PARTITION BY city ORDER BY MONTH(enrollment_date) ASC) AS amt_diff
+          FROM
+              customer_loyalty_history
+          WHERE
+                YEAR(enrollment_date) = 2018
+            AND MONTH(enrollment_date) IN (2, 3)
+          GROUP BY 1, 2)
+SELECT
+    city,
+    amt_diff
+FROM
+    cte
+WHERE
+     amt_diff = (SELECT MAX(amt_diff) FROM cte)
+  OR amt_diff = (SELECT MIN(amt_diff) FROM cte);
+
+
+
+-- 11. What was the quarterly Churn Rate during 2018?
+
+-- Common Table Expression (CTE) for calculating churn rate by quarter
+WITH quarterly_data AS
+    (SELECT
+        2018 AS 'year',
+        'Q1' AS 'quarter',
+        COUNT(*) AS enrolled_at_start_of_quarter,
+        SUM(IF(cancellation_date BETWEEN '2018-01-01' AND '2018-03-31', 1, 0)) AS canceled_in_quarter
+    FROM
+        customer_loyalty_history
+    WHERE
+        enrollment_date <= '2018-01-01'
+    UNION ALL
+    SELECT
+        2018 AS 'year',
+        'Q2' AS 'quarter',
+        COUNT(*) AS enrolled_at_start_of_quarter,
+        SUM(IF(cancellation_date BETWEEN '2018-04-01' AND '2018-06-30', 1, 0)) AS canceled_in_quarter
+    FROM
+        customer_loyalty_history
+    WHERE
+        enrollment_date <= '2018-04-01'
+    UNION ALL
+    SELECT
+        2018 AS 'year',
+        'Q3' AS 'quarter',
+        COUNT(*) AS enrolled_at_start_of_quarter,
+        SUM(IF(cancellation_date BETWEEN '2018-07-01' AND '2018-09-30', 1, 0)) AS canceled_in_quarter
+    FROM
+        customer_loyalty_history
+    WHERE
+        enrollment_date <= '2018-07-01'
+    UNION ALL
+    SELECT
+        2018 AS 'year',
+        'Q4' AS 'quarter',
+        COUNT(*) AS enrolled_at_start_of_quarter,
+        SUM(IF(cancellation_date BETWEEN '2018-10-01' AND '2018-12-31', 1, 0)) AS canceled_in_quarter
+    FROM
+        customer_loyalty_history
+    WHERE
+        enrollment_date <= '2018-10-01')
+-- Final SELECT statement to calculate the churn rate
+SELECT
+    year,
+    quarter,
+    enrolled_at_start_of_quarter,
+    canceled_in_quarter,
+    (canceled_in_quarter / enrolled_at_start_of_quarter) * 100 AS churn_rate
+FROM
+    quarterly_data;
---
+++ Findings-Analysis.md
@@ -0,0 +1,45 @@
+<h1 align="center">Findings & Analysis</h1>
+
+
+
+<p align="center">Note: Click the Tableau link above a chart to view a detailed, interactive version.</p> 
+
+## 3. Which locations do our most valuable customers come from?
+
+Tableau - [Link](https://public.tableau.com/views/Workbook2_CustomerFlightsProject/Map?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link)
+
+![Map](https://github.com/user-attachments/assets/115efc3e-3fb9-4696-a1ba-d6d672342e0c)
+
+- On average, the highest lifetime value customers are coming from the Eastern Canadian provinces of **Quebec, Newfoundland and Labrador, and New Brunswick**.
+  
+- Customers with the lowest average lifetime value are primarily located in the western territory of **Yukon**. This is largely due to the territory's unique economic and logistical challenges, including its remote location, high transportation costs, and elevated prices for goods and services. As a result, it tends to have a higher Cost of Living Index compared to many other Canadian provinces.
+
+- **Recommendation**: Consider retailoring marketing strategies towards lower valued customers. Create targeted promotions, region-specific offers, or localized advertising to help extend lifetime value. 
+
+## 4. What impact did the 2018 promotion have on the amount of enrollment compared to previous years?
+
+Tableau - [Link](https://public.tableau.com/views/Workbook2_CustomerFlightsProject/GroupedBarChart2?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link)
+
+![Grouped Bar Chart #2](https://github.com/rml-lee/MYSQL-Tableau-Customer-Flights-Project/assets/160198611/aae5f1e2-57e3-4110-967e-edb059aa61dd)
+
+- The 2018 promotion campaign occurred from February to the end of April. Without the promotion program, we would normally receive around 200 enrollments. 
+
+- The 2018 promotion campaign overall was a success, netting an additional 100 to 150 new customers per month while it was active.
+
+- April was the strongest month during the promotion, which yielded a total of 346 new customers. 
+
+- **Recommendations**: Continue to implement the promotion program annually and explore new creative ways to advertise and market it to attract new customers.
+
+
+## 11. What was the quarterly Churn Rate during 2018?
+
+Tableau - [Link](https://public.tableau.com/views/Workbook2_CustomerFlightsProject/DualCombination?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link)
+
+![Dual Combination](https://github.com/rml-lee/MYSQL-Tableau-Customer-Flights-Project/assets/160198611/02ff4248-7db4-453a-90a1-80d579626c3f)
+
+- The churn rate overall has been relatively low, hovering around 1% throughout the year of 2018. This indicates that there’s a high level of customer retention.
+  
+- The small decline in churn rate during the second quarter is likely due to the amount of new customers obtained during the 2018 promotion.
+  
+- **Recommendations**: Continue to understand the customer. Continuously gather and analyze customer feedback to understand their needs, preferences, and grievances. Use surveys, focus groups, and social media listening tools. This will help maintain and potentially improve churn rate.
+
